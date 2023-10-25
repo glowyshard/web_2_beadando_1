d323c0ece9c7d7819467303135012ebce1ec0cd3
glowyshard <norbi21212@gmail.com>
2023-10-25 11:33:50 +0200

    Fixed-Navbar Added-Logo

--- config.inc.php
+++ config.inc.php
@@ -1,5 +1,5 @@
 <?php 
 
 $menu = array(
-    array('text' => 'Home', 'link' => '/web_2_beadando_1/index.php')
+    array('text' => 'Home', 'link' => '/web_2_beadando_1-main/index.php')
 );
--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src="media/favicon.ico" alt="favicon" height="24" width="24">
+      <img src="media/logo.ico" alt="The Sweet Oven" height="60" width="60">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
