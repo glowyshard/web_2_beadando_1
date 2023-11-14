a501ce21eaf859ee0bd2be9295d38b971d215747
glowyshard <norbi21212@gmail.com>
2023-11-14 11:39:31 +0100

    CSS

--- fiok.php
+++ fiok.php
@@ -62,14 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <style>
-        .center-form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            margin-top: 50px; 
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">    
 </head>
 <body>
     <?php require_once("header.php")?>
